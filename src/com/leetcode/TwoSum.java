ec43d86c75476f14be1e68cbdf4ff06ace2e1053
Ku <r0905121198@gmail.com>
2024-05-19 15:05:36 +0800

    leetCode two sum updated
    1. 補上邏輯描述

--- src/com/leetcode/TwoSum.java
+++ src/com/leetcode/TwoSum.java
@@ -16,6 +16,10 @@ public class TwoSum {
 
     /**
      * time complexity: O(n + m). n為number length; Hash為O(1), m從Map中搜尋的次數且 m < n
+     * self explanation: 只需一次for loop, 使用map把current數字跟target數字做比對確認,
+     * 因數字是一對, 只有兩個數字, 所以只需要找到當前數字跟另一筆數字相加結果會為target
+     * 所以使用map儲存每筆數字所差數字,也就是target - current, 若後續找的數字在map中
+     * 也就代表著current的數字有跟map中有對應的數字, 就可直接回傳結果.
      *
      */
     public static int[] twoSum(int[] numbers, int target) {
